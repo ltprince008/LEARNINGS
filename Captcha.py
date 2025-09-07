f4bf4a89a637a77f49ce38b028b04702cf40da58
Pranav Verma <pv9085@srmist.edu.in>
2025-09-07 10:01:11 +0530

    Add files via upload
    through this code you can generate a  random captcha image in ".png" format

---
+++ Captcha.py
@@ -0,0 +1,28 @@
+# Install captcha library before running:
+# pip install captcha pillow
+
+from captcha.image import ImageCaptcha
+import random
+from PIL import Image
+
+# Specify the image size
+image = ImageCaptcha(width=450, height=100)
+
+# Generate random captcha text
+def generate_random_captcha_text(length=6):
+    characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    captcha_text = ''.join(random.choice(characters) for _ in range(length))
+    return captcha_text
+
+# Get random captcha text
+captcha_text = generate_random_captcha_text()
+
+# Generate the image of the given text
+data = image.generate(captcha_text)
+
+# Write the image on the given file and save it
+image.write(captcha_text, 'CAPTCHA1.png')
+
+# Open the saved captcha image
+img = Image.open('CAPTCHA1.png')
+img.show()
